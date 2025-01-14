7b20ad494c1a210d113e0e585dc0597c03bc8bcb
Andre <andredeveloper44@gmail.com>
2025-01-14 02:39:34 -0300

    novas alterações

--- bot/codigos/blacklist.js
+++ bot/codigos/blacklist.js
@@ -214,7 +214,8 @@ export const blacklist = [
     '5514991368989@s.whatsapp.net',
     '554298298257@s.whatsapp.net',
     '5515996654434@s.whatsapp.net',
-    '559991525856@s.whatsapp.net'
+    '559991525856@s.whatsapp.net',
+    '5521987923441@s.whatsapp.net'
 ];
 
 const BATCH_SIZE = 500; // Tamanho de cada lote para processamento da blacklist
